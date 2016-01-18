f9af4a27e8ef388c99e16bc338fda9f201908c9f
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2016-01-18 21:13:01 +0100

    move to gtk 2.0 ... add first steps

--- src/a20_sdk_builder.c
+++ src/a20_sdk_builder.c
@@ -23,86 +23,81 @@
 #include "libservice.h"
 
 
+// all used widgets
+GtkWidget *window;
+GtkWidget *vbox;
+GtkWidget *statusbar;
+GtkWidget *textfield;
+
+
+
 static void
-clone_button_clicked(GtkWidget *widget,
-	       gpointer data)
+clone_button_clicked(GtkWidget *widget, gpointer data)
 {
 	g_print(_("Your're in %s of %s\n"), __FUNCTION__, __FILE__);
 
 }
 
 static void
-download_button_clicked(GtkWidget *widget,
-	       gpointer data)
+download_button_clicked(GtkWidget *widget, gpointer data)
 {
 	g_print(_("Your're in %s of %s\n"), __FUNCTION__, __FILE__);
 
 }
 
 static void
-exit_button_clicked(GtkWidget *widget,
-	       gpointer data)
+exit_button_clicked(GtkWidget *widget, gpointer data)
 {
 	g_print(_("Your're in %s of %s\n"), __FUNCTION__, __FILE__);
 
 }
 
 
+/*
+ * build the gui ...
+ */
 static void
-activate(GtkApplication *app, gpointer user_data)
+build_main_window()
 {
-	GtkWidget *window;
-	GtkWidget *exit_button;
-	GtkWidget *clone_button;
-	GtkWidget *download_button;
-	GtkWidget *button_box;
-
-	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "A20-SDK-Builder");
-	//gtk_window_set_default_size(GTK_WINDOW(window), 600, 300);
-	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-
-
-	/*
-	 * the button box@button
-	 */
-	button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-	gtk_container_add(GTK_CONTAINER(window), button_box);
-
-	/*
-	 * clone a20_sdk git repo button
-	 */
-	clone_button = gtk_button_new_with_label(_("Clone A20_SDK"));
-	g_signal_connect(clone_button, "clicked", G_CALLBACK(clone_button_clicked), NULL);
-	// function pointer to func in libservice.a
-	//g_signal_connect_swapped(clone_button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-	gtk_container_add(GTK_CONTAINER(button_box), clone_button);
-
-	/*
-	 * download toolchain button
-	 */
-	download_button = gtk_button_new_with_label(_("Download A20_SDK"));
-	g_signal_connect(download_button, "clicked", G_CALLBACK(download_button_clicked), NULL);
-	// function pointer to func in libservice.a
-	//g_signal_connect_swapped(download_button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-	gtk_container_add(GTK_CONTAINER(button_box), download_button);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	
-	/*
-	 * exit button
-	 */
-	exit_button = gtk_button_new_with_label(_("Leave/Exit"));
-	g_signal_connect(exit_button, "clicked", G_CALLBACK(exit_button_clicked), NULL);
-	g_signal_connect_swapped(exit_button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-	gtk_container_add(GTK_CONTAINER(button_box), exit_button);
+	gtk_window_set_title(GTK_WINDOW(window), "A20-SDK-Builder");
+	gtk_window_set_default_size(GTK_WINDOW(window), 600, 300);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	//gtk_window_set_icon(GTK_WINDOW(window),create_pixbuf("a20_sdk_icon.png"));
+	GTK_WINDOW(window)->allow_shrink = TRUE;
 
-	gtk_widget_show_all(window);
+        // create vbox and add it to main window
+	vbox = gtk_vbox_new(FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+
+
+	// ------------------ hbox ------------------------
+	
+
+	// ------------------ textfield -------------------
+	textfield = gtk_text_view_new();
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(textfield), FALSE);
+	gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textfield), FALSE);
+
+	gtk_box_pack_start(GTK_BOX(vbox), textfield, TRUE, TRUE, 3);
+	
+	
+	// ------------------ statusbar -------------------
+	statusbar = gtk_statusbar_new();
+	gtk_box_pack_start(GTK_BOX(vbox), statusbar, FALSE, TRUE, 1);
+
+	
+	// -------------- connect signals -----------------
+	g_signal_connect_swapped(G_OBJECT(window), "destroy",
+				 G_CALLBACK(gtk_main_quit),
+				 G_OBJECT(window));
 }
 
 
 int
 main(int argc, char **argv)
 {
-	GtkApplication *app;
 	int status;
 
 	setlocale(LC_ALL, "");
@@ -121,11 +116,20 @@ main(int argc, char **argv)
 	/*
 	 * init gtk stuff
 	 */
-	app = gtk_application_new("none.none.a20_sdk_builder", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	if (!g_thread_supported())
+		g_thread_init(NULL);
 
-	status = g_application_run(G_APPLICATION(app), argc, argv);
-	g_object_unref (app);
+	gdk_threads_init();
+	gdk_threads_enter();
+
+	gtk_init(&argc, &argv);
+	
+	build_main_window();
+	
+	gtk_widget_show_all(window);  
+	
+	gtk_main();
+	gdk_threads_leave();
 	
 	return status;
 }
