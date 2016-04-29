fc27efcd3e97523415a87c35480d4bfa825f7fb0
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2016-04-29 21:42:28 +0200

    add libbaalue some minor changes

--- do_all_in_one_step.sh
+++ do_all_in_one_step.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#sudo make uninstall
-sudo make distclean
+make distclean
 
 autoreconf --install || exit 1
-./configure --enable-debug CC=clang --prefix=$HOME --sysconfdir=/etc
-#./configure --enable-debug --prefix=$HOME --sysconfdir=/etc
-#./configure --prefix=$HOME --sysconfdir=/etc
+#./configure --enable-debug CC=clang --prefix=$HOME --sysconfdir=/etc
+./configure --enable-debug --prefix=$HOME --sysconfdir=/etc
 
 cd po
 make update-po
@@ -16,7 +14,7 @@ make
 sudo make install
 
 # start conf daemon
-xterm -fn "-misc-fixed-medium-r-normal--14-*-*-*-*-*-iso8859-15" -geometry 120x40 -hold -e "sdk_confd -f a20_sdk_builder.conf" &
+# xterm -fn "-misc-fixed-medium-r-normal--14-*-*-*-*-*-iso8859-15" -geometry 120x40 -hold -e "sdk_confd -f a20_sdk_builder.conf" &
 
 # start gui
 xterm -fn "-misc-fixed-medium-r-normal--14-*-*-*-*-*-iso8859-15" -geometry 120x40 -hold -e "sdk_builder -f a20_sdk_builder.conf" &
